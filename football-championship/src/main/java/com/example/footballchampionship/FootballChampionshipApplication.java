25328613370c2566b79eff6b27624819e8ab48bb
SSzeWen <seetszewen@gmail.com>
2024-09-13 20:49:28 +0800

    Create React Frontend and SpringBoot Backend

---
+++ football-championship/src/main/java/com/example/footballchampionship/FootballChampionshipApplication.java
@@ -0,0 +1,33 @@
+package com.example.footballchampionship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballChampionshipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FootballChampionshipApplication.class, args);
+	}
+
+	private void addTeam() {
+	}
+
+	private void addMatch() {
+	}
+
+	private void viewRankings() {
+	}
+
+	private void viewTeamDetails() {
+	}
+
+	private void editTeam() {
+	}
+
+	private void editMatch() {
+	}
+
+	private void clearData() {
+	}
+
+}
